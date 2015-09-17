0ec4b8eba7f431cc2364ecec48aa03221ed87f52
Hamish <hamish.ogilvy@gmail.com>
2015-09-17 16:15:21 +1000

    Formatted, random fixes and dodgy comment removal

--- porterstemmer_fixes_test.go
+++ porterstemmer_fixes_test.go
@@ -1,22 +1,15 @@
 package porterstemmer
 
-
-
 import (
-	"fmt"
 	"testing"
 )
-
-
 
 // Test for issue listed here:
 // https://github.com/reiver/go-porterstemmer/issues/1
 //
 // StemString("ion") was causing runtime exception
 func TestStemStringIon(t *testing.T) {
-
 	expected := "ion"
-
 	s := "ion"
 	actual := StemString(s)
 	if expected != actual {
